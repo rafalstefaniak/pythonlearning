dfe774782cfcd246597de802311c7ac41b410819
rafalstefaniak <rafalstefaniak@outlook.com>
2017-12-12 11:16:51 +0100

    Initial commit

---
+++ R01C02.py
@@ -0,0 +1,6 @@
+# R01C02
+# Rafał Stefaniak 12.12.2017
+
+print("Rafał Stefaniak")
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
---
+++ R01C03.py
@@ -0,0 +1,6 @@
+# R01C03
+
+print("'Think Different'")
+print("\nSteve Jobs")
+
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
